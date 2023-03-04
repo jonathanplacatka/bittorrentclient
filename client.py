48505f8d4abc352212d1e774c6e3fa887c9986e8
jplac <jplacatka@live.ca>
2023-03-04 16:44:13 -0600

    send tracker response

--- client.py
+++ client.py
@@ -1,9 +1,9 @@
 import torrent
 import random
+import requests
+import bdecode
 
-t = torrent.Torrent('oz.torrent')
-print(t.info_hash)
-print(t.length)
+PORT = 6881
 
 #20 bytes: 2 for client id, 4 for version number, remaining are random integers
 def generate_peer_id():
@@ -13,6 +13,32 @@ def generate_peer_id():
     return id
 
 def get_peer_list():
-    params = {}
+    params = {
+        'info_hash': torrent_file.info_hash,
+        'peer_id': peer_id,
+        'port': PORT,
+        'uploaded': 0,
+        'downloaded': 0,
+        'left': torrent_file.length,
+        'compact': 1,
+        'event': 'started'
+    }
 
-print(generate_peer_id())
+    url = torrent_file.data['announce']
+
+    response = requests.get(url, params)
+    
+    response_content = bdecode.decode(response.content)
+    print(response_content)
+
+def decode_peer_list():
+   peers = b'\x8e\xa0k\x0e\x1a\xe1\xd43\x89\xb4\xc8\xd5\x18\xd9}i\xc0\x08'
+   print(len(peers))
+
+#torrent_file = torrent.Torrent('oz.torrent')
+#peer_id = generate_peer_id()
+
+#get_peer_list()
+decode_peer_list()
+
+
