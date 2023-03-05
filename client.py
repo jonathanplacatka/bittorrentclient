2d5000dfa364fdd895caf8ea8ec7c5fe53d70cfa
jplac <jplacatka@live.ca>
2023-03-04 18:37:01 -0600

    decode compact peer lists

--- client.py
+++ client.py
@@ -7,12 +7,12 @@ PORT = 6881
 
 #20 bytes: 2 for client id, 4 for version number, remaining are random integers
 def generate_peer_id():
-    id = 'SY1000' 
+    id = 'PY1000' 
     for k in range(0, 14):
         id += str(random.randint(0,9))
     return id
 
-def get_peer_list():
+def tracker_request():
     params = {
         'info_hash': torrent_file.info_hash,
         'peer_id': peer_id,
@@ -25,20 +25,27 @@ def get_peer_list():
     }
 
     url = torrent_file.data['announce']
-
     response = requests.get(url, params)
-    
-    response_content = bdecode.decode(response.content)
-    print(response_content)
+    return bdecode.decode(response.content)
 
-def decode_peer_list():
-   peers = b'\x8e\xa0k\x0e\x1a\xe1\xd43\x89\xb4\xc8\xd5\x18\xd9}i\xc0\x08'
-   print(len(peers))
+#TODO: check for compact vs non-compact peer list
+def decode_compact_peer_list(peer_bytes):
 
-#torrent_file = torrent.Torrent('oz.torrent')
-#peer_id = generate_peer_id()
+    peer_list = []
+    for x in range(0, len(peer_bytes), 6):
+        ip = '.'.join(str(x) for x in peer_bytes[x:x+4]) #first 4 bytes are ip
+        port = int.from_bytes(peer_bytes[x+4:x+6], byteorder='big') #last 2 bytes together are port
+        peer_list.append('{}:{}'.format(ip, port))
 
-#get_peer_list()
-decode_peer_list()
+    return peer_list
+
+
+
+torrent_file = torrent.Torrent('oz.torrent')
+peer_id = generate_peer_id()
+tracker_response = tracker_request()
+
+print(tracker_response)
+print(decode_compact_peer_list(tracker_response['peers']))
 
 
