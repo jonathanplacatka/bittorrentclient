786e5a2777637ff01c7dc4a14d4a5a506f897327
jplac <jplacatka@live.ca>
2023-03-09 11:03:52 -0600

    handshake with peers

--- client.py
+++ client.py
@@ -2,8 +2,10 @@ import torrent
 import random
 import requests
 import bdecode
+import socket
+import select
 
-PORT = 6881
+LISTEN_PORT = 7000
 
 #20 bytes: 2 for client id, 4 for version number, remaining are random integers
 def generate_peer_id():
@@ -16,7 +18,7 @@ def tracker_request():
     params = {
         'info_hash': torrent_file.info_hash,
         'peer_id': peer_id,
-        'port': PORT,
+        'port': LISTEN_PORT,
         'uploaded': 0,
         'downloaded': 0,
         'left': torrent_file.length,
@@ -25,27 +27,80 @@ def tracker_request():
     }
 
     url = torrent_file.data['announce']
+        
     response = requests.get(url, params)
+
     return bdecode.decode(response.content)
 
 #TODO: check for compact vs non-compact peer list
 def decode_compact_peer_list(peer_bytes):
-
     peer_list = []
     for x in range(0, len(peer_bytes), 6):
         ip = '.'.join(str(x) for x in peer_bytes[x:x+4]) #first 4 bytes are ip
         port = int.from_bytes(peer_bytes[x+4:x+6], byteorder='big') #last 2 bytes together are port
-        peer_list.append('{}:{}'.format(ip, port))
+        peer_list.append((ip, port)) 
 
     return peer_list
 
+def connect_peers():
+    for peer in peer_list:   
+        p_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        p_socket.setblocking(False)
+        peer_sockets.append(p_socket)
+        try:
+            if p_socket.connect_ex(peer) == 0:
+                p_socket.append(p_socket)
+        except Exception as e:
+            print('exception', e)
+
+def send_handshake(peer_socket):
+    HANDSHAKE = b'\x13BitTorrent protocol\x00\x00\x00\x00\x00\x00\x00\x00'
+    msg = HANDSHAKE + torrent_file.info_hash + peer_id.encode()
+
+    peer_socket.setblocking(True)
+    peer_socket.sendall(msg)
+    data = peer_socket.recv(1024)
+
+    print("received:", data)
+
+def run():
+    while True:
+        try:
+            readable, writable, exceptions = select.select(
+                [],
+                peer_sockets,
+                []
+            )
+
+            for sock in writable:
+                if sock.getsockopt(socket.SOL_SOCKET, socket.SO_ERROR) == 0:
+                    send_handshake(sock)
+                    peer_sockets.remove(sock)
+                    connected.append(sock)
+                    print("CONNECTIONS:", len(connected))
+                else:
+                    peer_sockets.remove(sock)
+        except Exception as e:
+            print('exception', e)
 
 
-torrent_file = torrent.Torrent('oz.torrent')
+peer_sockets = []
+connected = []
+
+torrent_file = torrent.Torrent('o.torrent')
 peer_id = generate_peer_id()
 tracker_response = tracker_request()
 
 print(tracker_response)
-print(decode_compact_peer_list(tracker_response['peers']))
+
+peer_list = decode_compact_peer_list(tracker_response['peers'])
+print(peer_list)
+
+connect_peers()
+run()
+
+
+
+
 
 
