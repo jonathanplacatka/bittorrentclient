5f01188524405bb9933b7626bf57116a3e9915e0
jplac <jplacatka@live.ca>
2023-03-12 23:09:46 -0500

    recieve handshake and bitfield

--- client.py
+++ client.py
@@ -64,29 +64,55 @@ def send_handshake(peer_socket):
     HANDSHAKE = b'\x13BitTorrent protocol\x00\x00\x00\x00\x00\x00\x00\x00'
     msg = HANDSHAKE + torrent_file.info_hash + peer_id.encode()
     peer_socket.sendall(msg)
-    #data = peer_socket.recv(1024)
-    #print("received:", data)
-    #print("received handshake")
 
-def recieve_message(peer_socket):
-    data = peer_socket.recv(1024)
+def recieve_message(data, peer_socket):
+    #get peer object from list
+    peer_ip = peer_socket.getpeername()
+    peer = peer_list[peer_list.index(peer_ip)] 
+    peer.buffer += data
 
-    if data:
-        #get peer object from list
-        peer_ip = peer_socket.getpeername()
-        peer = peer_list[peer_list.index(peer_ip)] 
-
-        print("message receieved")
+    if peer.handshake == False:
+        recieve_handshake(peer)
     else:
-        connected.remove(peer_socket)
+        message_id = peer.buffer[4]
+        if message_id == 5:
+            recieve_bitfield(peer)
+        else:
+            print("RECEIVED MESSAGE ID: ", message_id)
 
+def recieve_handshake(peer):
+    if len(peer.buffer) >= 68:
+        if peer.buffer[0:20] == b'\x13BitTorrent protocol' and peer.buffer[28:48] == torrent_file.info_hash:
+            print(peer.buffer[0:68])
+            print("RECEIVED: HANDSHAKE")
+            peer.handshake = True
+            peer.buffer = peer.buffer[68:]
+        else:
+            print("invalid peer handshake, dropping connection")
+
+def recieve_bitfield(peer):
+    bitfield_length = int.from_bytes(peer.buffer[0:4], byteorder='big')
+    print("BITFIELD LENGTH: ", bitfield_length)
+    if len(peer.buffer) > bitfield_length:
+        bitfield = peer.buffer[5:bitfield_length]
+
+        print(bitfield)
+        print("RECIEVED: BITFIELD")
+
+        print(len(bitfield)*8)
+        peer.buffer = peer.buffer[bitfield_length:]
+        
 def run():
     while True:
         try:
             readable, writable, exceptions = select.select(connected, connecting, [])
 
             for sock in readable:
-                    recieve_message(sock)
+                data = sock.recv(8192)
+                if data:
+                    recieve_message(data, sock)
+                else:
+                    connected.remove(sock)
 
             for sock in writable:
                 if sock in connecting:
@@ -96,17 +122,24 @@ def run():
                         connected.append(sock)
                         send_handshake(sock)
                         print("{} connected: {}".format(len(connected), sock))
+
+                        #connecting.clear() #TESTING, REMOVE ME
                     else:
                         connecting.remove(sock)
 
         except Exception as e:
             print('exception', e)
 
+
 class Peer:
     def __init__(self, ip, port):
         self.address = (ip, port)
+        self.handshake = False
+        self.buffer = b''
+
     def __eq__(self, obj):
         return self.address == obj
+
 
 peer_list = []
 
@@ -115,6 +148,7 @@ connected = []
 
 torrent_file = torrent.Torrent('mint.torrent')
 peer_id = generate_peer_id()
+
 tracker_response = tracker_request()
 print(tracker_response)
 
@@ -126,6 +160,9 @@ print()
 
 connect_peers()
 run()
+
+
+
 
 
 
