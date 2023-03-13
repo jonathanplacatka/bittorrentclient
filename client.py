398d51471489860ba9284b046c3576c7588c0145
jplac <jplacatka@live.ca>
2023-03-12 19:48:33 -0500

    cleanup connection code

--- client.py
+++ client.py
@@ -54,44 +54,46 @@ def connect_peers():
     for peer in peer_list:   
         p_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         p_socket.setblocking(False)
-        peer_sockets.append(p_socket)
-        try:
-            if p_socket.connect_ex(peer) == 0:
-                p_socket.append(p_socket)
-        except Exception as e:
-            print('exception', e)
+        errcode = p_socket.connect_ex(peer)
+        
+        if errcode == 115: #connection in progress     
+            connecting.append(p_socket)
+        elif errcode == 0: #connected
+            connected.append(p_socket)
 
 def send_handshake(peer_socket):
     HANDSHAKE = b'\x13BitTorrent protocol\x00\x00\x00\x00\x00\x00\x00\x00'
     msg = HANDSHAKE + torrent_file.info_hash + peer_id.encode()
-
-    peer_socket.setblocking(True)
     peer_socket.sendall(msg)
-    data = peer_socket.recv(1024)
 
+    data = peer_socket.recv(1024)
     #print("received:", data)
     print("received handshake")
 
 def run():
     while True:
         try:
-            readable, writable, exceptions = select.select([], peer_sockets, [])
+            readable, writable, exceptions = select.select([], connecting, [])
 
             for sock in writable:
-                if sock.getsockopt(socket.SOL_SOCKET, socket.SO_ERROR) == 0:
-                    send_handshake(sock)
-                    peer_sockets.remove(sock)
-                    connected.append(sock)
-                    print("{} connected: {}".format(len(connected), sock))
-                else:
-                    peer_sockets.remove(sock)
+                if sock in connecting:
+                    if sock.getsockopt(socket.SOL_SOCKET, socket.SO_ERROR) == 0:
+                    
+                        sock.setblocking(True)
+                        connecting.remove(sock)
+                        connected.append(sock)
+                        send_handshake(sock)
+
+                        print("{} connected: {}".format(len(connected), sock))
+                    else:
+                        connecting.remove(sock)
         except Exception as e:
             print('exception', e)
 
-peer_sockets = []
+connecting = []
 connected = []
 
-torrent_file = torrent.Torrent('bl.torrent')
+torrent_file = torrent.Torrent('mint.torrent')
 peer_id = generate_peer_id()
 tracker_response = tracker_request()
 print(tracker_response)
